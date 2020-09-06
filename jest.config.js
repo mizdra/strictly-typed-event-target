2b3550ed50cf4f691aaf80094f1f14a939fd3a45
mizdra <pp.mizdra@gmail.com>
2020-09-06 19:54:48 +0900

    test: add test for node

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,26 @@
-module.exports = {
+// @ts-check
+
+/** @type import('@jest/types').Config.InitialOptions */
+const SHARED_CONFIG = {
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+};
+
+/** @type import('@jest/types').Config.InitialOptions */
+module.exports = {
+  projects: [
+    {
+      ...SHARED_CONFIG,
+      displayName: 'node',
+      testEnvironment: 'node',
+    },
+    {
+      ...SHARED_CONFIG,
+      displayName: 'browser',
+      testEnvironment: 'jsdom',
+    },
+  ],
 };
