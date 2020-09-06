9a08a45eefb84904f5704cd6fe1b42da227481c6
mizdra <pp.mizdra@gmail.com>
2020-09-06 18:58:13 +0900

    test: fix implicit any warning

--- test-d/helper.ts
+++ test-d/helper.ts
@@ -4,9 +4,9 @@ export interface FooEventMap {
 }
 
 /** T can be instantiated */
-export declare function canInstantiate<T>();
+export declare function canInstantiate<T>(): void;
 
 /** U can assign to T (`const T = U`) */
-export declare function canAssign<T, U extends T>();
+export declare function canAssign<T, U extends T>(): void;
 
-export declare function expectType<T, U extends T>();
+export declare function expectType<T, U extends T>(): void;
