0ee60505e539897ef9fe206829ab241306ce939d
mizdra <pp.mizdra@gmail.com>
2020-09-06 22:24:04 +0900

    refactor: rename `test-d` to `test-type`

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   root: true,
   extends: ['@mizdra/mizdra', '@mizdra/mizdra/+typescript', '@mizdra/mizdra/+prettier'],
   parserOptions: {
-    project: ['./tsconfig.src.json', './tsconfig.test.json', './tsconfig.test-d.json'],
+    project: ['./tsconfig.src.json', './tsconfig.test.json', './tsconfig.test-type.json'],
   },
   env: {
     es6: true,
