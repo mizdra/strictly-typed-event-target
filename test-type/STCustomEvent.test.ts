c9032ad89ec3433063e31b8a5a7096a6e0fa70fc
mizdra <pp.mizdra@gmail.com>
2021-02-11 13:20:18 +0900

    refactor: fix lint error and tsc error

--- test-type/STCustomEvent.test.ts
+++ test-type/STCustomEvent.test.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import { STCustomEvent } from '../src';
 import { canInstantiate, FooEventMap, canAssign } from './helper';
 
--- test-type/helper.ts
+++ test-type/helper.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 export interface FooEventMap {
   onmessage: string;
   onerror: Error;
